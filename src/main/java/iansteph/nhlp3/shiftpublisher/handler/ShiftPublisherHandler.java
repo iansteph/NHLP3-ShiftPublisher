2593c2335f05cb39713eecfa88d7b8eb8b0d02e7
Ian Stephenson 🦈🏒 <ianp.stephenson@yahoo.com>
2020-06-27 15:07:10 -0700

    Add LambdaLogger
    - Following the AWS Docs: https://docs.aws.amazon.com/lambda/latest/dg/java-logging.html

--- src/main/java/iansteph/nhlp3/shiftpublisher/handler/ShiftPublisherHandler.java
+++ src/main/java/iansteph/nhlp3/shiftpublisher/handler/ShiftPublisherHandler.java
@@ -3,8 +3,12 @@ package iansteph.nhlp3.shiftpublisher.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import iansteph.nhlp3.shiftpublisher.model.request.ShiftPublisherRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class ShiftPublisherHandler implements RequestHandler<ShiftPublisherRequest, Object> {
+
+    private static final Logger logger = LogManager.getLogger(ShiftPublisherHandler.class);
 
     public Object handleRequest(final ShiftPublisherRequest shiftPublisherRequest, final Context context) {
         return "Hello, world! 🌍";
